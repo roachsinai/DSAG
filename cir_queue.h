00e6c56a7ed87b7acc5bf9a568fe62ad8c21446a
Roach Sinai <710347541@qq.com>
2016-01-23 16:52:38 +0800

    sublime_text3 change tab to spaces.

--- cir_queue.h
+++ cir_queue.h
@@ -3,10 +3,10 @@
 typedef int Element;
 struct Cir_Queue
 {
-	Element *pBase;
-	int front;  // 指向队列第一个元素
-	int rear;  // 指向队列最后一个元素后一个位置
-	int maxSize;  // 循环队列开辟的空间
+    Element *pBase;
+    int front;  // 指向队列第一个元素
+    int rear;  // 指向队列最后一个元素后一个位置
+    int maxSize;  // 循环队列开辟的空间
 }Cir_Queue, *p_Cir_Queue;
 
 void CreateCir_Queue(Cir_Queue Q, int maxSize);
